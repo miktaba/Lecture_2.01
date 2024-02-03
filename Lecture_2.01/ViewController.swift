1e42735fb8d7a7eedf00a73940e8abc3815a3fd9
miktaba <mtabakaev@gmail.com>
2024-02-03 21:34:53 +0100

    Work with logic

--- Lecture_2.01/ViewController.swift
+++ Lecture_2.01/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    // private let cornerRadius: CGFloat = 10
+    // если прописываем доп. логику
+    
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var greetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        greetingLabel.isHidden.toggle()
+        greetingButton.layer.cornerRadius = 10 // = cornerRadius
+        
     }
 
-
+    @IBAction func greetingButtonDidTapped(_ sender: UIButton) {
+        greetingLabel.isHidden.toggle()
+        sender.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+            for: .normal
+        )
+// Заменил тернарным оператором
+//        if greetingLabel.isHidden {
+//            sender.setTitle("Show Greeting", for: .normal)
+//        } else {
+//            sender.setTitle("Hide Greetings", for: .normal)
+//        }
+    }
+    
 }
 
